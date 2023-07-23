921881d850d6b8e6178b2f4cf5ba719336169688
Kevin Putrajaya <freakperzon@yahoo.com>
2023-07-23 09:38:46 +0700

    Multi-bill resolution

--- site.js
+++ site.js
@@ -10,144 +10,186 @@
 
   // Alpine data
   document.addEventListener('alpine:init', () => {
-    Alpine.data('spill', () => ({
-      total: '73150',
-      items: '30000 ari\n27500 roy\n9000 ari roy roy',
-      error: null,
-      parsedData: null,
+    Alpine.data('spill', function () {
+      return {
+        // Data
+        total: '73150',
+        items: '30000 ani\n27500 roy\n9000 ani roy roy',
+        error: null,
+        billData: null,
+        mbrData: this.$persist({ people: [], bills: [], map: {} }),
 
-      compute() {
-        try {
-          // Parse total
-          const total = Math.floor(this.total.trim());
-          if (!(total > 0)) {
-            throw Error('Total is invalid');
-          }
+        // Actions
+        compute() {
+          try {
+            // Parse total
+            const total = Math.floor(this.total.trim());
+            if (!(total > 0)) {
+              throw Error('Total is invalid');
+            }
 
-          // Parse items
-          const peopleTotal = {};
-          let proratedTotal = 0;
-          const items = this.items
-            .split('\n')
-            .map((item) => item.trim().toUpperCase())
-            .filter((item) => item)
-            .map((item, itemIndex) => {
-              const args = item
-                .split(' ')
-                .map((arg) => arg.trim())
-                .filter((arg) => arg);
+            // Parse items
+            const peopleTotal = {};
+            let proratedTotal = 0;
+            const items = this.items
+              .split('\n')
+              .map((item) => item.trim().toUpperCase())
+              .filter((item) => item)
+              .map((item, itemIndex) => {
+                const args = item
+                  .split(' ')
+                  .map((arg) => arg.trim())
+                  .filter((arg) => arg);
 
-              // Parse price
-              const price = Math.floor(args[0]);
-              if (!(price > 0)) {
-                throw Error(`Item ${itemIndex + 1} has invalid price`);
-              }
+                // Parse price
+                const price = Math.floor(args[0]);
+                if (!(price > 0)) {
+                  throw Error(`Item ${itemIndex + 1} has invalid price`);
+                }
 
-              // Parse people
-              const people = args.slice(1);
-              if (args.length < 2) {
-                throw Error(`Item ${itemIndex + 1} has no person name`);
-              }
-              const proratedPrice = Math.round(price / people.length);
-              proratedTotal += proratedPrice * people.length;
-              const result = {
-                no: itemIndex + 1,
-                price: price,
-                priceWithFee: null,
-                people: {},
-              };
-              people.forEach((person) => {
-                result.people[person] = (result.people[person] || 0) + proratedPrice;
-                peopleTotal[person] = 0;
+                // Parse people
+                const people = args.slice(1);
+                if (args.length < 2) {
+                  throw Error(`Item ${itemIndex + 1} has no person name`);
+                }
+                const proratedPrice = Math.round(price / people.length);
+                proratedTotal += proratedPrice * people.length;
+                const result = {
+                  no: itemIndex + 1,
+                  price: price,
+                  priceWithFee: null,
+                  people: {},
+                };
+                people.forEach((person) => {
+                  result.people[person] = (result.people[person] || 0) + proratedPrice;
+                  peopleTotal[person] = 0;
+                });
+                return result;
               });
-              return result;
+            if (!items.length) {
+              throw Error('Items are empty');
+            }
+
+            // Calculate and apply fee
+            let totalPrice = 0;
+            let totalPriceWithFee = 0;
+            const feePercentage = (total - proratedTotal) / proratedTotal;
+            items.forEach((item) => {
+              item.priceWithFee = Math.round(item.price * (1 + feePercentage));
+              Object.keys(item.people).forEach((person) => {
+                item.people[person] = Math.round(item.people[person] * (1 + feePercentage));
+                peopleTotal[person] += item.people[person];
+              });
+              totalPrice += item.price;
+              totalPriceWithFee += item.priceWithFee;
             });
-          if (!items.length) {
-            throw Error(`Items are empty`);
+
+            // Save to bill data
+            this.error = null;
+            this.billData = {
+              people: Object.keys(peopleTotal).sort(),
+              feePercentage: Math.round(feePercentage * 1000) / 10,
+              items,
+              totalPrice,
+              totalPriceWithFee,
+              peopleTotal,
+            };
+            this.setQueryParams({
+              total: this.total,
+              items: this.items,
+            });
+          } catch (e) {
+            this.error = e.message;
+            this.billData = null;
+          }
+        },
+        async share() {
+          // Share link using Web Share API
+          if (navigator.share) {
+            await navigator.share({ url: location.href });
+          }
+        },
+        mbrSave() {
+          if (!this.billData || !this.billData.people.length) return;
+
+          // Ask for payer's name
+          const nameList = this.billData.people.map((person, index) => `${index + 1}. ${person}`).join('\n');
+          const payerInput = prompt('Who paid this bill?\n' + nameList);
+          const payer = this.billData.people[Math.floor(payerInput) - 1];
+          if (!payer) {
+            if (payerInput) {
+              alert(`Please input number 1 - ${this.billData.people.length}!`);
+            }
+            return;
           }
 
-          // Calculate and apply fee
-          let totalPrice = 0;
-          let totalPriceWithFee = 0;
-          const feePercentage = (total - proratedTotal) / proratedTotal;
-          items.forEach((item) => {
-            item.priceWithFee = Math.round(item.price * (1 + feePercentage));
-            Object.keys(item.people).forEach((person) => {
-              item.people[person] = Math.round(item.people[person] * (1 + feePercentage));
-              peopleTotal[person] += item.people[person];
+          // Record bill into MBR data
+          this.mbrData.people = [...new Set([...this.mbrData.people, ...this.billData.people])].sort();
+          this.mbrData.bills.push({
+            payer,
+            totalPriceWithFee: this.billData.totalPriceWithFee,
+            peopleTotal: this.billData.peopleTotal,
+          });
+
+          // Recalculate payer-payee map
+          this.mbrData.map = {};
+          this.mbrData.bills.forEach((bill) => {
+            this.mbrData.map[bill.payer] = this.mbrData.map[bill.payer] || {};
+            Object.keys(bill.peopleTotal).forEach((person) => {
+              this.mbrData.map[bill.payer][person] = this.mbrData.map[bill.payer][person] || 0;
+              this.mbrData.map[bill.payer][person] += bill.peopleTotal[person];
             });
-            totalPrice += item.price;
-            totalPriceWithFee += item.priceWithFee;
           });
+        },
+        mbrClear() {
+          this.mbrData = { people: [], bills: [], map: {} };
+        },
+        mbrCompute(payer, payee) {
+          const pay = (this.mbrData.map[payee] || {})[payer] || 0;
+          const receive = (this.mbrData.map[payer] || {})[payee] || 0;
+          return pay - receive;
+        },
 
-          this.error = null;
-          this.parsedData = {
-            people: Object.keys(peopleTotal).sort(),
-            feePercentage: Math.round(feePercentage * 1000) / 10,
-            items,
-            totalPrice,
-            totalPriceWithFee,
-            peopleTotal,
-          };
-          this.setQueryParams({
-            total: this.total,
-            items: this.items,
-          });
-        } catch (e) {
-          this.error = e.message;
-          this.parsedData = null;
-        }
-      },
-      select(e) {
-        e.target.select();
-      },
-      formatNumber(num) {
-        return num != null ? num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',') : '';
-      },
+        // Helpers
+        select(e) {
+          e.target.select();
+        },
+        formatNumber(num) {
+          return num != null ? num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',') : '';
+        },
+        getQueryParams() {
+          const params = {};
+          const search = window.location.search;
+          if (search) {
+            search
+              .substring(1)
+              .split('&')
+              .forEach((param) => {
+                const [key, value] = param.split('=');
+                params[key] = decodeURIComponent(value);
+              });
+          }
+          return params;
+        },
+        setQueryParams(params) {
+          const search = Object.keys(params)
+            .map((key) => `${key}=${encodeURIComponent(params[key])}`)
+            .join('&');
+          window.history.replaceState(null, null, `?${search}`);
+        },
+        initQueryParams() {
+          const params = this.getQueryParams();
+          this.total = params.total || this.total;
+          this.items = params.items || this.items;
+        },
 
-      // Share link using Web Share API
-      async share(evt) {
-        if (navigator.share) {
-          await navigator.share({
-            url: location.href,
-          });
-        }
-      },
-
-      getQueryParams() {
-        const params = {};
-        const search = window.location.search;
-        if (search) {
-          search
-            .substr(1)
-            .split('&')
-            .forEach((param) => {
-              const [key, value] = param.split('=');
-              params[key] = decodeURIComponent(value);
-            });
-        }
-        return params;
-      },
-
-      setQueryParams(params) {
-        const search = Object.keys(params)
-          .map((key) => `${key}=${encodeURIComponent(params[key])}`)
-          .join('&');
-        window.history.replaceState(null, null, `?${search}`);
-      },
-
-      initParams() {
-        const params = this.getQueryParams();
-        this.total = params.total || this.total;
-        this.items = params.items || this.items;
-      },
-
-      init() {
-        this.initParams();
-        this.compute();
-        this.$watch('total', () => this.compute());
-        this.$watch('items', () => this.compute());
-      },
-    }));
+        init() {
+          this.initQueryParams();
+          this.compute();
+          this.$watch('total', () => this.compute());
+          this.$watch('items', () => this.compute());
+        },
+      };
+    });
   });
 })();
