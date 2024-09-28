ebcb304cd7b767d62308f37989a197cb2e57e2cb
Kevin Putrajaya <freakperzon@yahoo.com>
2024-09-28 18:40:22 +0700

    Change file input

--- site.js
+++ site.js
@@ -214,8 +214,7 @@
         uploadImage() {
           const fileInput = document.createElement('input');
           fileInput.type = 'file';
-          fileInput.accept = 'image/*';
-          fileInput.capture = 'environment';
+          fileInput.accept = 'capture=camera,image/*';
           fileInput.style.display = 'none';
           document.body.appendChild(fileInput);
 
